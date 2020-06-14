412191f68b656071f82f116ca5edda49ea968e62
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-06-14 13:53:33 -0700

    Update encapsulation.py
    Resolved

--- encapsulation.py
+++ encapsulation.py
@@ -1,18 +1,20 @@
 # Title:        Encapsulation
 
 class employee:
-    def __init__(self, num, name, sal):
-        self._num = 'number' #protected attribute
-        self._name = 'name' #protected attribute
-        self.__sal = 'salary' #private attribute
+    def __init__(self, name, sal):
+        self._name = name #protected attribute
+        self.__sal = sal #private attribute
+
+    def pSal(self):
+        print("An employee makes " + self.__sal)
+
+        
 
 
 
 
 
 if __name__ == "__main__":
-    e1 =  employee('4536',"JOE",'1000')
-    e1._num = 4536
-    e1.__sal = 1000
-    print(e1.__sal, e1._num)
+     e1 = employee('JOE','1000')
+     e1.pSal()
     
